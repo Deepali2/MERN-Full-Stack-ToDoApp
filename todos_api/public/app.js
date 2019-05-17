a68308939a1aa9c43bfe1fa38be5be7f52560090
Deepali2 <hdgarg@gmail.com>
2019-05-17 19:46:02 +0300

    connects the form to the api

--- todos_api/public/app.js
+++ todos_api/public/app.js
@@ -8,18 +8,37 @@ $(document).ready(function() {
   .catch(function(err) {
     alert(`There is an error: ${err}`)
   })
+  $('#todoInput').keypress(function() {
+    if (event.which === 13) createTodo(); // 13 is the keycode for the enter key
+  })
 })
 
+//displays all the todos on the page
 function addTodos(todos) {
-  todos.forEach(function(todo) {
-    console.log(todo.name);
+  todos.forEach(todo => addTodo(todo))  
+}
+
+//helper function to append a todo item to the list
+function addTodo(todo) {
     // let newTodo = $(`<li class="task">${todo.name}<li>`); // do not use this as it is adding an extra dot for each list
-    let newTodo = $('<li class="task">' + todo.name + '</li>');
+   let newTodo = $('<li class="task">' + todo.name + '</li>');
     // newTodo.addClass('task'); could also do it this way
     if (todo.completed === true) {
       newTodo.addClass('done');
     }
     $('.list').append(newTodo);
+}
+
+//creates a new Todo and displays it
+function createTodo() {
+  var userInput = $('#todoInput').val();  //gives us the value in the input
+  $.post('/api/todos', {name: userInput})
+  .then(todo => {
+    $('#todoInput').val(''); 
+    addTodo(todo);
+  })
+  .catch (function(err) {
+    console.log(err);
   })
 }
 
