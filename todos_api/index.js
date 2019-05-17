0e6622eab171df3e9154631f89bee44b25fb6b00
Deepali2 <hdgarg@gmail.com>
2019-05-18 01:13:05 +0300

    ensures that the front end and the back end are running on different ports

--- todos_api/index.js
+++ todos_api/index.js
@@ -1,7 +1,8 @@
 const express = require ('express'),
-          app = express(),
+   app        = express(),
    todoRoutes = require('./routes/todos'),
-   bodyParser = require('body-parser');
+   bodyParser = require('body-parser'),
+   PORT       = process.env.PORT||3001;
 
 app.use(express.static(__dirname + '/public')); //renders the css file to the index.html file
 app.use(express.static(__dirname + '/views'));//renders the index.html page in the views directory
@@ -15,7 +16,7 @@ app.get('/', function(req, res) {
 
 app.use('/api/todos', todoRoutes); // sets the '/' to mean '/api/todos'
 
-const PORT = process.env.PORT||3000;
+
 app.listen(PORT, function() {
   console.log(`todosApi node listening on port ${PORT}`)
 });
