ac9f560b66f1af0fb277e15e63e053fb39359173
Deepali2 <hdgarg@gmail.com>
2019-05-17 12:04:48 +0300

    implements the show route

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-
+//Index Route
 router.get('/', function(req, res) {
   db.Todo.find()
   .then(function(todos) {
@@ -13,10 +13,21 @@ router.get('/', function(req, res) {
   })
 })
 
+//Create Route
 router.post('/', function(req, res) {
   db.Todo.create(req.body)//create the new todo
   .then(function(newTodo) {//return the newly cerated to do item
     res.status(201).json(newTodo);
+  })
+  .catch(function(err) {
+    res.send(err);
+  })
+})
+//Show Route
+router.get('/:todoId', function(req, res) {
+  db.Todo.findById(req.params.todoId)
+  .then(function(foundTodo) {
+    res.json(foundTodo)
   })
   .catch(function(err) {
     res.send(err);
