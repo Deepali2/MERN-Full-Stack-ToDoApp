e0e358ce6016866c35d31e23eb05e52a59c70d7b
Deepali2 <hdgarg@gmail.com>
2019-05-22 19:15:16 -0700

    adds the mLab database

--- todos_api/models/index.js
+++ todos_api/models/index.js
@@ -1,7 +1,10 @@
 var mongoose = require ('mongoose');
 mongoose.set('debug', true);
 
-mongoose.connect('mongodb://localhost/todo-api', {useNewUrlParser:true});
+// mongoose.connect('mongodb://localhost/todo-api', {useNewUrlParser:true});
+mongoose.connect('mongodb://deepali:Mango2018deployWebpages@ds137100.mlab.com:37100/reacttodolist', {useNewUrlParser:true});
+
 mongoose.Promise = Promise; // to allow us to to use promise syntax
 
 module.exports.Todo = require('./todo');
+
